bcf1e416269e45c9297f0d90ad985a54be911117
Abhinav Dhasmana <abhinav.dhasmana@mfine.co>
2019-12-03 09:27:41 +0530

    day2 part 1

---
+++ 2019/2/partOne.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const data = fs.readFileSync(path.resolve(__dirname, 'partOneData.txt')).toString().split(',');
+const intData = data.map((val) => parseInt(val, 10));
+console.log(data.length, intData.length);
+
+
+for (let i = 0; i < intData.length; i += 4) {
+  if (intData[i] === 99) {
+    break;
+  } else if (intData[i] === 1) {
+    intData[intData[i + 3]] = intData[intData[i + 1]] + intData[intData[i + 2]];
+  } else if (intData[i] === 2) {
+    intData[intData[i + 3]] = intData[intData[i + 1]] * intData[intData[i + 2]];
+  } else {
+    console.log('something went wrong', intData[i]);
+  }
+}
+console.log(intData);
