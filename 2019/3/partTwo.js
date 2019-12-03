cf10da7472fe41e4b9370c2aca23eb1e564e7596
Abhinav Dhasmana <abhinav.dhasmana@mfine.co>
2019-12-03 23:23:10 +0530

    Day 3 part 2

--- 2019/3/partTwo.js
+++ 2019/3/partTwo.js
@@ -4,7 +4,7 @@ const path = require('path');
 const [dataOne, dataTwo] = fs.readFileSync(path.resolve(__dirname, 'partOneData.txt')).toString().split('\n');
 const dataOneArray = dataOne.split(',');
 const dataTwoArray = dataTwo.split(',');
-
+let dataOneSteps = 0;
 const pointsTouchedByA = {};
 let currentPosition = [0, 0];
 const intersectionPoints = [];
@@ -13,60 +13,73 @@ for (let i = 0; i < dataOneArray.length; i += 1) {
   const value = parseInt(dataOneArray[i].substring(1), 10);
   if (direction === 'R') {
     for (let j = 1; j <= value; j += 1) {
-      pointsTouchedByA[`${currentPosition[0] + j}_${currentPosition[1]}`] = true;
+      dataOneSteps += 1;
+      pointsTouchedByA[`${currentPosition[0] + j}_${currentPosition[1]}`] = { dataOneSteps };
     }
     currentPosition = [currentPosition[0] + value, currentPosition[1]];
   } else if (direction === 'L') {
     for (let j = 1; j <= value; j += 1) {
-      pointsTouchedByA[`${currentPosition[0] - j}_${currentPosition[1]}`] = true;
+      dataOneSteps += 1;
+      pointsTouchedByA[`${currentPosition[0] - j}_${currentPosition[1]}`] = { dataOneSteps };
     }
     currentPosition = [currentPosition[0] - value, currentPosition[1]];
   } else if (direction === 'U') {
     for (let j = 1; j <= value; j += 1) {
-      pointsTouchedByA[`${currentPosition[0]}_${currentPosition[1] + j}`] = true;
+      dataOneSteps += 1;
+      pointsTouchedByA[`${currentPosition[0]}_${currentPosition[1] + j}`] = { dataOneSteps };
     }
     currentPosition = [currentPosition[0], currentPosition[1] + value];
   } else if (direction === 'D') {
     for (let j = 1; j <= value; j += 1) {
-      pointsTouchedByA[`${currentPosition[0]}_${currentPosition[1] - j}`] = true;
+      dataOneSteps += 1;
+      pointsTouchedByA[`${currentPosition[0]}_${currentPosition[1] - j}`] = { dataOneSteps };
     }
     currentPosition = [currentPosition[0], currentPosition[1] - value];
   }
 }
 
 currentPosition = [0, 0];
+let dataTwoSteps = 0;
 for (let i = 0; i < dataTwoArray.length; i += 1) {
   const direction = dataTwoArray[i][0];
   const value = parseInt(dataTwoArray[i].substring(1), 10);
   if (direction === 'R') {
     for (let j = 1; j <= value; j += 1) {
+      dataTwoSteps += 1;
       const pos = `${currentPosition[0] + j}_${currentPosition[1]}`;
       if (pointsTouchedByA[pos]) {
-        intersectionPoints.push(pos);
+        pointsTouchedByA[pos].dataTwoSteps = dataTwoSteps;
+        intersectionPoints.push(pointsTouchedByA[pos]);
       }
     }
     currentPosition = [currentPosition[0] + value, currentPosition[1]];
   } else if (direction === 'L') {
     for (let j = 1; j <= value; j += 1) {
+      dataTwoSteps += 1;
       const pos = `${currentPosition[0] - j}_${currentPosition[1]}`;
       if (pointsTouchedByA[pos]) {
-        intersectionPoints.push(pos);
+        pointsTouchedByA[pos].dataTwoSteps = dataTwoSteps;
+        intersectionPoints.push(pointsTouchedByA[pos]);
       }
     }
     currentPosition = [currentPosition[0] - value, currentPosition[1]];
   } else if (direction === 'U') {
     for (let j = 1; j <= value; j += 1) {
+      dataTwoSteps += 1;
       const pos = `${currentPosition[0]}_${currentPosition[1] + j}`;
       if (pointsTouchedByA[pos]) {
-        intersectionPoints.push(pos);
+        pointsTouchedByA[pos].dataTwoSteps = dataTwoSteps;
+        intersectionPoints.push(pointsTouchedByA[pos]);
       }
     }
     currentPosition = [currentPosition[0], currentPosition[1] + value];
   } else if (direction === 'D') {
     for (let j = 1; j <= value; j += 1) {
+      dataTwoSteps += 1;
       const pos = `${currentPosition[0]}_${currentPosition[1] - j}`;
       if (pointsTouchedByA[pos]) {
-        intersectionPoints.push(pos);
+        pointsTouchedByA[pos].dataTwoSteps = dataTwoSteps;
+        intersectionPoints.push(pointsTouchedByA[pos]);
       }
     }
     currentPosition = [currentPosition[0], currentPosition[1] - value];
@@ -75,8 +88,7 @@ for (let i = 0; i < dataTwoArray.length; i += 1) {
 let min = Infinity;
 console.log(intersectionPoints);
 intersectionPoints.forEach((val) => {
-  const [l, r] = val.split('_');
-  const sum = Math.abs(parseInt(l, 10)) + Math.abs(parseInt(r, 10));
+  const sum = val.dataOneSteps + val.dataTwoSteps;
   // console.log(sum);
   if (sum < min) {
     console.log(sum, val);
